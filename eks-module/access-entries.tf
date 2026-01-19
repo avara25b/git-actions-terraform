218aacaa19d2fdb41c809f52bd9af48326f05715
avara25b <andrii.var@edu.312school.com>
2026-01-19 16:58:09 -0500

    added eks access entry for gha

--- eks-module/access-entries.tf
+++ eks-module/access-entries.tf
@@ -17,3 +17,23 @@ resource "aws_eks_access_policy_association" "andrii_user_view" {
     type = "cluster"
   }
 }
+
+data "aws_iam_roles" "github-actions-iam-role" {
+  name_regex = "^github-actions-iam-role-for-runner-.*"
+}
+
+resource "aws_eks_access_entry" "github_actions_iam_role_access" {
+  cluster_name  = aws_eks_cluster.kubernetes_cluster.name
+  principal_arn = tolist(data.aws_iam_roles.github-actions-iam-role.arns)[0]
+  type          = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "github_actions_iam_role_policy" {
+  cluster_name  = aws_eks_cluster.kubernetes_cluster.name
+  principal_arn = tolist(data.aws_iam_roles.github-actions-iam-role.arns)[0]
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
+}
